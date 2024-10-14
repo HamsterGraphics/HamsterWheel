585eb2cfa9f357c9b47f858332a12c828681053b
HamsterGraph <jiajun.coding@gmail.com>
2024-10-15 01:06:39 +0800

    wip

---
+++ Source/OS/Public/IWindow.h
@@ -0,0 +1,27 @@
+﻿/*
+ * Copyright (c) 2024-2025 HamsterGraphics
+ *
+ * https://github.com/HamsterGraphics/HamsterWheel
+ *
+ */
+
+#pragma once
+
+#include <Math/Rect.h>
+
+typedef struct WindowDesc
+{
+	const char* Title;
+	void* Handle;
+	Rect WindowRect;
+
+	bool Maxmized : 1;
+	bool Minimized : 1;
+	bool FullScreen : 1;
+	bool Borderless : 1;
+} WindowDesc;
+
+typedef struct MonitorDesc
+{
+	Rect MonitorRect;
+} MonitorDesc;
