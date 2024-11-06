293b7dde53e5c76d281c54a56d8b9cf4e018b6c9
HamsterGraph <jiajun.coding@gmail.com>
2024-11-06 16:53:03 +0800

    watch stl allocation

--- Source/Core/Public/STL/Allocator.h
+++ Source/Core/Public/STL/Allocator.h
@@ -7,6 +7,10 @@
 
 #pragma once
 
+#include "../Base/Assert.h"
+
+#include <atomic>
+#include <format>
 #include <iostream>
 #include <memory>
 
@@ -17,10 +21,25 @@ template<typename T>
 class STLAllocator : public std::allocator<T> 
 {
 public:
-    T* allocate(size_t n)
+    T* allocate(const size_t n)
     {
-        std::cout << "[STL Allocator] Request to allocate " << n << std::endl;
+        allocated_size() += n;
+        std::cout << std::format("[STL Allocator] Allocate {} bytes, Totally {} bytes.", n, allocated_size()).data() << std::endl;
         return std::allocator<T>::allocate(n);
+    }
+
+    void deallocate(T* const ptr, const size_t n)
+    {
+        Assert(allocated_size() >= n);
+        allocated_size() -= n;
+        std::cout << std::format("[STL Allocator] Deallocate {} bytes, Totally {} bytes.", n, allocated_size()).data() << std::endl;
+        std::allocator<T>::deallocate(ptr, n);
+    }
+
+    size_t& allocated_size()
+    {
+        static size_t s_allocatedBytes;
+        return s_allocatedBytes;
     }
 };
 
